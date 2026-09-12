2a92f7d9149c8d6a14d30b2845c49665d4c83399
Ravindu Thiranjaya <thiranjayaravindu@gmail.com>
2026-09-13 01:01:49 +0530

    Add Context and Settings classes for MongoDB integration

---
+++ API/Data/MongoDbContext.cs
@@ -0,0 +1,22 @@
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+using API.Settings;
+
+namespace API.Data
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IOptions<MongoDbSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            _database = client.GetDatabase(settings.Value.DatabaseName);
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string collectionName)
+        {
+            return _database.GetCollection<T>(collectionName);
+        }
+    }
+}
---
+++ API/Settings/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+namespace API.Settings
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
